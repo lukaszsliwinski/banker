a8aec14f47bed7b2e50c78cffc2d00b343248952
lukaszsliwinski <lukaszsliwinski0@gmail.com>
2022-03-23 20:26:48 +0100

    filter imported data and send to template as list

--- app/views.py
+++ app/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
 import requests
+from bs4 import BeautifulSoup
+from datetime import datetime
 
 def index(request):
     url = 'https://www.bankier.pl/gielda/notowania/akcje'
     page = requests.get(url)
-    return render(request, 'app/index.html', {'data': page.text})
+
+    soup = BeautifulSoup(page.content, 'html.parser')
+    walors = soup.find_all('td', class_='colWalor')
+    prices = soup.find_all('td', class_='colKurs')
+    data = []
+    for walor, price in zip(walors, prices):
+        name = walor.find('a').get('title')
+        code = walor.find('a').text
+        price = price.text
+        date = datetime.now().strftime('%H:%M:%S %Y-%m-%d')
+        data.append([name, code, price, date])
+
+    return render(request, 'app/index.html', {'data': data})
