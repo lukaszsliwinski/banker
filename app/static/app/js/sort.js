6c79c6652217c440a7d2ebcf22df81f98acd9c56
lukaszsliwinski <lukaszsliwinski0@gmail.com>
2022-03-25 20:12:53 +0100

    Replace ascSort descSort and sort functions by one sort function in sort.js file

--- app/static/app/js/sort.js
+++ app/static/app/js/sort.js
@@ -2,52 +2,31 @@ const table = document.getElementById('table');
 const rows = table.rows
 
 
-// ascending sort function
-const ascSort = () => {
-    let sorted = false;
-    while (!sorted) {
-        sorted = true;
-        for (let i = 1; i < (rows.length - 1); i++) {
-            // get two adjacent elements
-            let element1 = rows[i].getElementsByTagName('td')[0].innerHTML.toLowerCase();
-            let element2 = rows[i+1].getElementsByTagName('td')[0].innerHTML.toLowerCase();
-            // replace if necessary
-            if (element1 > element2) {
-                rows[i].parentNode.insertBefore(rows[i+1], rows[i]);
-                sorted = false;
-            };
-        };
-    };
-};
-
-
-// descending sort function
-const descSort = () => {
-    let sorted = false;
-    while (!sorted) {
-        sorted = true;
-        for (let i = 1; i < (rows.length - 1); i++) {
-            // get two adjacent elements
-            let element1 = rows[i].getElementsByTagName('td')[0].innerHTML.toLowerCase();
-            let element2 = rows[i+1].getElementsByTagName('td')[0].innerHTML.toLowerCase();
-            // replace if necessary
-            if (element1 < element2) {
-                rows[i].parentNode.insertBefore(rows[i+1], rows[i]);
-                sorted = false;
-            };
-        };
-    };
-};
-
-
-// main sort function
+// sort function
 const sort = () => {
     // get sorting type from template
-    let selectedOption = document.getElementById('sorting_type').value;
+    let selectedOption = document.getElementById('sorting_type').value;    
 
-    if (selectedOption == 'asc') {
-        ascSort();
-    } else {
-        descSort();
+    let sorted = false;
+    
+    while (!sorted) {
+        sorted = true;
+        for (let i = 1; i < (rows.length - 1); i++) {
+            // get two adjacent elements
+            let element1 = rows[i].getElementsByTagName('td')[0].innerHTML.toLowerCase();
+            let element2 = rows[i+1].getElementsByTagName('td')[0].innerHTML.toLowerCase();
+            // check sorting type and replace rows if necessary
+            if (selectedOption == 'asc') {
+                if (element1 > element2) {
+                    rows[i].parentNode.insertBefore(rows[i+1], rows[i])
+                    sorted = false;
+                };
+            } else {
+                if (element1 < element2) {
+                    rows[i].parentNode.insertBefore(rows[i+1], rows[i])
+                    sorted = false;
+                };
+            };
+        };
     };
 };
