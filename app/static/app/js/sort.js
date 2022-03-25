b990924c39cd07d2601473d7f73a8f8a80d9cb05
lukaszsliwinski <lukaszsliwinski0@gmail.com>
2022-03-25 19:37:01 +0100

    Add comments

--- app/static/app/js/sort.js
+++ app/static/app/js/sort.js
@@ -2,13 +2,16 @@ const table = document.getElementById('table');
 const rows = table.rows
 
 
+// ascending sort function
 const ascSort = () => {
     let sorted = false;
     while (!sorted) {
         sorted = true;
         for (let i = 1; i < (rows.length - 1); i++) {
+            // get two adjacent elements
             let element1 = rows[i].getElementsByTagName('td')[0].innerHTML.toLowerCase();
             let element2 = rows[i+1].getElementsByTagName('td')[0].innerHTML.toLowerCase();
+            // replace if necessary
             if (element1 > element2) {
                 rows[i].parentNode.insertBefore(rows[i+1], rows[i]);
                 sorted = false;
@@ -18,24 +21,28 @@ const ascSort = () => {
 };
 
 
+// descending sort function
 const descSort = () => {
     let sorted = false;
     while (!sorted) {
         sorted = true;
         for (let i = 1; i < (rows.length - 1); i++) {
+            // get two adjacent elements
             let element1 = rows[i].getElementsByTagName('td')[0].innerHTML.toLowerCase();
             let element2 = rows[i+1].getElementsByTagName('td')[0].innerHTML.toLowerCase();
+            // replace if necessary
             if (element1 < element2) {
                 rows[i].parentNode.insertBefore(rows[i+1], rows[i]);
                 sorted = false;
             };
-    }   ;
+        };
     };
 };
 
 
 // main sort function
 const sort = () => {
+    // get sorting type from template
     let selectedOption = document.getElementById('sorting_type').value;
 
     if (selectedOption == 'asc') {
--- app/views.py
+++ app/views.py
@@ -5,26 +5,36 @@ from datetime import datetime
 from .models import StockQuotes
 
 def index(request):
+    # import data from website
     url = 'https://www.bankier.pl/gielda/notowania/akcje'
     page = requests.get(url)
 
+    # create object with 
     soup = BeautifulSoup(page.content, 'html.parser')
+
+    # find walors and prices
     walors = soup.find_all('td', class_='colWalor')
     prices = soup.find_all('td', class_='colKurs')
-    data = []
 
+    # clear database
     StockQuotes.objects.all().delete()
 
+    # create list for data
+    data = []
     for walor, price in zip(walors, prices):
-        
+        # get content from tags
         name = walor.find('a').get('title')
         code = walor.find('a').text
         price = price.text
+
+        # get actual date
         date = datetime.now().strftime('%H:%M:%S %Y-%m-%d')
         
+        # send data to database
         stock_quotes = StockQuotes(name=name, code=code, price=price, date=date)
         stock_quotes.save()
         
+        # add row of data to list
         data.append([name, code, price, date])
 
     return render(request, 'app/index.html', {'data': data})
